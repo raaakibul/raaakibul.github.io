2aa63a4afa7b0d86bc5faa481ae35fec809df143
rakibul-islam <raakibul.uap@gmail.com>
2024-12-16 18:01:59 +0600

    personal website updated

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// javascript code
+// Toggle the navigation menu in mobile view
+function toggleMenu() {
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('active');
+}
+
+// Toggle between dark and light themes
+function toggleTheme() {
+    const body = document.body;
+    const homeSection = document.getElementById('home');
+
+    body.classList.toggle('dark-mode');
+    homeSection.classList.toggle('dark-mode');
+}
