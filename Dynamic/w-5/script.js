e211c400b6171ef5bdf9de7b9a6950a2a6d6f2f6
rakibul-islam <raakibul.uap@gmail.com>
2024-12-16 17:40:01 +0600

    another website design added

---
+++ Dynamic/w-5/script.js
@@ -0,0 +1,14 @@
+// Toggle the navigation menu in mobile view
+function toggleMenu() {
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('active');
+}
+
+// Toggle between dark and light themes
+function toggleTheme() {
+    const body = document.body;
+    const homeSection = document.getElementById('home');
+
+    body.classList.toggle('dark-mode');
+    homeSection.classList.toggle('dark-mode');
+}
