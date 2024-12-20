9696c662feaedf3f9218eccc6628ad3479c73c7d
rakibul-islam <raakibul.uap@gmail.com>
2024-12-20 22:56:31 +0600

    website added

---
+++ Dynamic/w-8/script.js
@@ -0,0 +1,9 @@
+// script.js
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
