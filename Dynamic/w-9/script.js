44ff798dcb6d2de570ccd6c41d2d52eddb18e748
rakibul-islam <raakibul.uap@gmail.com>
2024-12-21 22:15:41 +0600

    website added

---
+++ Dynamic/w-9/script.js
@@ -0,0 +1,7 @@
+// Toggle Mobile Menu
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
